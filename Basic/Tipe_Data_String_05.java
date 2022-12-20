6b70663ec642cac52186d26cc2de1b687243eab7
riana lestari <lestaririanaa@gmail.com>
2022-12-20 22:08:29 +0700

    Tipe data string

---
+++ Basic/Tipe_Data_String_05.java
@@ -0,0 +1,16 @@
+public class Tipe_Data_String_05 {
+    public static void main(String[] args) {
+
+        String firstName = "Riana";
+        String lastName = "Lestari";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        // Menggabungkan String
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+
+    }
+}
