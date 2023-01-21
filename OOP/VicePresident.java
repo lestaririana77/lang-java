6e38d649011e464533f7333d3d9e8896a59c1c7f
lestaririanaa@gmail.com <lestaririana77>
2023-01-21 18:10:12 +0700

    Method Overriding

--- OOP/VicePresident.java
+++ OOP/VicePresident.java
@@ -1,3 +1,10 @@
 class VicePresident extends Manager { // class child (turunan)
 
+    // Method Overriding = method yang mendeklarasiin ulang method yg terdapat di parent ke child
+    // semua nya harus sama dari return atau tidaknya, nama method nya dan parameternya harus sama
+    // Yg boleh dibedakan hanya body dari isi si method
+    void sayHello(String name){
+        System.out.println("Hi " + name + ", My Name is VP " + this.name);
+    }
+
 }
